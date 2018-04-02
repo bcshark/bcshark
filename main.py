c919850647e22e06fa47a48f9b0e11c3f13941ee
Yang Yun <igouzy@live.com>
2018-04-03 01:21:26 +0800

    basic architecture of data collectors

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import logging
+import threading
 
 from collectors.collector_factory import collector_factory
+
+TIMEOUT_COLLECT_IN_SECONDS = 60
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -12,7 +15,14 @@ if __name__ == '__main__':
     settings = { 'logger': logger }
     factory = collector_factory(settings)
 
+    threads = []
     collectors = factory.get_all_collectors()
     for collector in collectors:
-        collector.collect()
+        threads.append(threading.Thread(target=collector.collect))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join(TIMEOUT_COLLECT_IN_SECONDS)
 
