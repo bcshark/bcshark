537a5e6071ce0e8ea06bdc8544d2f1479f22ed12
Yang Yun <igouzy@live.com>
2018-04-10 14:08:20 +0800

    add okcoin collector

--- collectors/collector_factory.py
+++ collectors/collector_factory.py
@@ -2,6 +2,7 @@ from .huobi import collector_huobi
 from .binance import collector_binance
 from .okex import collector_okex
 from .poloniex import collector_poloniex
+from .okcoin import collector_okcoin
 
 class collector_factory(object):
     def __init__(this, settings):
@@ -18,6 +19,8 @@ class collector_factory(object):
             collector = collector_okex(this.settings)
         elif collector_name == 'poloniex':
             collector = collector_poloniex(this.settings)
+        elif collector_name == 'okcoin':
+            collector = collector_okcoin(this.settings)
 
         return collector
 
@@ -26,7 +29,8 @@ class collector_factory(object):
             this.get_collector('huobi'), 
             this.get_collector('binance'),
             this.get_collector('okex'),
-            this.get_collector('poloniex') 
+            this.get_collector('poloniex'),
+            this.get_collector('okcoin')
         ]
 
         return collectors
---
+++ collectors/okcoin.py
@@ -0,0 +1,66 @@
+import requests
+
+from model.market_tick import  market_tick
+from .collector import collector
+from .utility import *
+
+class collector_okcoin(collector):
+    API_URL = "https://www.okcoin.com/api/v1/%s"
+    DEFAULT_PERIOD = "1min"
+    DEFAULT_SIZE = 200
+
+    def __init__(this, settings):
+        super(collector_okcoin, this).__init__(settings)
+        this.period = this.DEFAULT_PERIOD
+        this.symbols_okcoin = this.symbols['okcoin']
+
+    def translate(this, objs):
+        ticks = []
+        for obj in objs:
+            tick = market_tick()
+            tick.timezone_offset = this.timezone_offset
+            tick.time = obj[0]
+            tick.open = obj[1]
+            tick.high = obj[2]
+            tick.low = obj[3]
+            tick.close = obj[4]
+            tick.volume = obj[5]
+            tick.amount = 0
+            tick.count = 0
+            tick.period = this.get_generic_period_name(this.period)
+
+            ticks.append(tick)
+
+        return ticks
+
+    def collect(this):
+        symbol_index = 0
+
+        time_second = int(time.time())
+        time_second = time_second - time_second % 60 - 300 ## possible to miss data each 5 mins ?
+        for symbol in this.symbols_okcoin:
+            symbol_index += 1
+
+            if symbol == "":
+                continue
+
+            url = "kline.do?symbol=%s&type=%s&size=%d&since=%d" % (symbol, this.DEFAULT_PERIOD, this.DEFAULT_SIZE, time_second)
+            url = this.API_URL % url
+            data = this.http_request_json(url, None)
+
+            if not data:
+                this.logger.error('cannot get response from okcoin (%s)' % symbol)
+                continue
+
+            ticks = this.translate(data)
+            this.bulk_save_ticks('okcoin', this.get_generic_symbol_name(symbol_index), ticks)
+
+            this.logger.info('get response from okcoin')
+
+    def get_generic_symbol_name(this, symbol_index):
+        symbols_default = this.symbols['default']
+
+        return symbols_default[symbol_index]
+
+    def get_generic_period_name(this, period_name):
+        return this.DEFAULT_PERIOD
