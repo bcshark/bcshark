5e924b62230cf40110183528a32293dddb7bff0f
lix <yanxianbin4@163.com>
2018-04-24 20:33:19 +0800

    k20 calculation

---
+++ collect_k20_index.py
@@ -0,0 +1,46 @@
+import logging
+import logging.handlers
+import threading
+
+from lib.config import ConfigurationManager
+from collectors.collector_factory import collector_factory
+from adapters.influxdb_adapter import influxdb_adapter
+from adapters.mysqldb_adapter import mysqldb_adapter
+
+TIMEOUT_COLLECT_IN_SECONDS = 60
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
+
+file_logger_handler = logging.handlers.TimedRotatingFileHandler('logs/collect.log', when = 'D', interval = 1, backupCount = 10)
+file_logger_handler.suffix = "%Y-%m-%d_%H-%M-%S.log"
+file_logger_handler.setFormatter(formatter)
+file_logger_handler.setLevel(logging.DEBUG)
+logger.addHandler(file_logger_handler)
+
+if __name__ == '__main__':
+    global factory
+
+    settings = ConfigurationManager('config/global.json')
+    settings['logger'] = logger
+    settings['db_adapter'] = influxdb_adapter(settings['influxdb'])
+    #settings['db_adapter'] = mysqldb_adapter(settings['mysqldb'])
+
+    factory = collector_factory(settings)
+    threads = []
+
+    settings['db_adapter'].open()
+
+    collectors = [factory.get_collector('k20_index_calc')]
+    for collector in collectors:
+        threads.append(threading.Thread(target=collector.collect_rest))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join(TIMEOUT_COLLECT_IN_SECONDS)
+
+    settings['db_adapter'].close()
+
