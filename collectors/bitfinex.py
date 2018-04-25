c9984ebed01cad04cd9ea33511c6609e136d08e2
Yang Yun <igouzy@live.com>
2018-04-26 00:09:40 +0800

    bitfinex support more symbols

--- collectors/bitfinex.py
+++ collectors/bitfinex.py
@@ -2,81 +2,95 @@ import requests
 import json
 import StringIO
 import gzip
+import re
 
 from model.market_tick import  market_tick
 from .collector import collector
 from .utility import *
 
 class collector_bitfinex(collector):
-    DEFAULT_PERIOD = "1min"
-    DEFAULT_SIZE = 200
-    DEFAULT_DELAY = 3
+	DEFAULT_PERIOD = "1min"
+	DEFAULT_SIZE = 200
+	PATTERN_KEY = 'trade:1m:(t[A-Z]+)'
 
-    def __init__(this, settings, market_settings):
-        super(collector_bitfinex, this).__init__(settings, market_settings)
+	def __init__(this, settings, market_settings):
+		super(collector_bitfinex, this).__init__(settings, market_settings)
 
-        this.period = this.DEFAULT_PERIOD
-        this.chanId = ''
+		this.period = this.DEFAULT_PERIOD
+		this.subscribed_channels = {}
+		this.symbols_bitfinex = this.symbols['bitfinex']
 
-    def translate(this, ts, obj):
-        tick = {
-            "time": obj[0]/1000,
-            "timezone_offset" : this.timezone_offset,
-            "open" : float(obj[1]),
-            "close" : float(obj[2]),
-            "low" : float(obj[4]),
-            "high" : float(obj[3]),
-            "amount" : float(0),
-            "volume" : float(obj[5]),
-            "count" : 0,
-            "period" : this.get_generic_period_name(this.period)
-        }
+	def translate(this, obj):
+		tick = {
+			"time": obj[0] / 1000,
+			"timezone_offset" : this.timezone_offset,
+			"open" : float(obj[1]),
+			"close" : float(obj[2]),
+			"low" : float(obj[4]),
+			"high" : float(obj[3]),
+			"amount" : float(0),
+			"volume" : float(obj[5]),
+			"count" : 0,
+			"period" : this.get_generic_period_name(this.period)
+		}
 
-        return [ tick ]
+		return tick
 
-    def on_open(this, websocket_client):
-        this.logger.info('bitfinex websocket connection established')
+	def on_open(this, websocket_client):
+		this.logger.info('bitfinex websocket connection established')
 
-        this.subscription_delay = this.DEFAULT_DELAY
+	def on_message(this, websocket_client, raw_message):
+		this.logger.info('receive message from bitfinex websocket: %s', raw_message)
+		
+		message_json = json.loads(raw_message)
 
-    def on_message(this, websocket_client, raw_message):
+		if isinstance(message_json, dict):
+			if message_json.has_key('platform'):
+				status = message_json['platform']['status']
 
-        this.logger.info('receive message from bitfinex websocket: %s', raw_message)
-        message_json = json.loads(raw_message)
-        if isinstance(message_json, dict):
-            if message_json.has_key('platform'):
-                status = message_json['platform']['status']
-                if status == 1:
-                    this.logger.info('bitfinex ws connected!')
-                    subscription_msg = {
-                        "event": "subscribe",
-                        "channel": "candles",
-                        "key": "trade:1m:tBTCUSD"
-                    }
-                    this.send_ws_message_json(subscription_msg)
-            elif message_json.has_key('event'):
-                event = message_json['event']
-                this.logger.info('event=%s',event)
-                if event == "subscribed":
-                    this.chanId = message_json['chanId']
-                    this.logger.info('bitfinex ws channel: %s', message_json['channel'])
-                    this.logger.info('bitfinex ws chanId: %s', this.chanId)
-                    this.logger.info('bitfinex ws key: %s', message_json['key'])
-        elif isinstance(message_json, list):
-            if message_json[0] == this.chanId:
-                data = message_json[1]
-                if isinstance(data[0],list):
-                    this.logger.info('bitfinex ws ignore history long candles for chanId: %s', this.chanId)
-                    return
-                if data != 'hb':
-                    ticks = this.translate(data[0], data)
-                    this.save_tick('bitfinex_ticks', 'bitfinex', 'btcusdt', ticks[0])
+				if status == 1:
+					for symbol in this.symbols_bitfinex:
+						subscription_msg = {
+							"event": "subscribe",
+							"channel": "candles",
+							"key": "trade:1m:%s" % symbol
+						}
+						this.send_ws_message_json(subscription_msg)
+			elif message_json.has_key('event'):
+				event = message_json['event']
+				if event == "subscribed":
+					match = re.search(this.PATTERN_KEY, message_json['key'])
 
-    def collect_ws(this):
-        this.start_listen_websocket(this.WS_URL, this)
+					if match:
+						symbol_name = match.group(1)
+						channel_id = str(message_json['chanId'])
+						this.subscribed_channels[channel_id] = symbol_name
+						this.logger.info('symbol %s subscribed, channel %s', symbol_name, channel_id)
+		elif isinstance(message_json, list):
+			channel_id = str(message_json[0])
+			if this.subscribed_channels.has_key(channel_id):
+				data = message_json[1]
+				symbol_name = this.subscribed_channels[channel_id]
+				if isinstance(data[0], list):
+					this.logger.info('bitfinex ws ignore history long candles for symbol: %s', symbol_name)
+				elif data != 'hb':
+					tick = this.translate(data)
+					this.save_tick('bitfinex_ticks', 'bitfinex', this.get_generic_symbol_name(symbol_name), tick)
 
-    def collect_rest(this):
-        this.logger.info('bitfinex rest api by pass!')
+	def collect_ws(this):
+		this.start_listen_websocket(this.WS_URL, this)
 
-    def get_generic_period_name(this, period_name):
-        return period_name
+	def collect_rest(this):
+		this.logger.info('bitfinex rest api by pass!')
+
+	def get_generic_symbol_name(this, symbol_name):
+		symbols_default = this.symbols['default']
+
+		for symbol_index in range(len(this.symbols_bitfinex)):
+			if this.symbols_bitfinex[symbol_index] == symbol_name:
+				return symbols_default[symbol_index]
+
+		return None
+
+	def get_generic_period_name(this, period_name):
+		return period_name
