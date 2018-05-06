e19bdcfee8df108d58bf2e9e84ccaa8d480ede10
Yang Yun <igouzy@live.com>
2018-05-06 18:11:34 +0800

    group by time

--- services/kline_service.py
+++ services/kline_service.py
@@ -8,14 +8,14 @@ class kline_service(object):
         return result_set.raw
 
     def get_tvkline_by_market_symbol(this, symbol, from_time, to_time, size):
-        sql = "select time, open, close, low, high from k10_index where time >= %d and time <= %d order by time desc limit %d" % (from_time * 1e9, to_time * 1e9, size)
+        sql = "select time, first(open) as open, last(close) as close, min(low) as low, max(high) as high from k10_index where time >= %d and time <= %d group by time(1d) order by time desc limit %d" % (from_time * 1e9, to_time * 1e9, size)
         rows = this.client.query(sql, epoch = 's')
         print "%d, %d rows: %d" % (from_time, to_time, len(rows))
         return rows 
 
     def get_kline_by_market_symbol(this, market, symbol, size):
         if market == 'market_index':
-            sql = "select time, open, close, low, high from k10_index order by time desc limit %d" % size
+            sql = "select time, first(open) as open, last(close) as close, min(low) as low, max(high) as high from k10_index group by time(1d) order by time desc limit %d" % size
         else:
             sql = "select time, open, close, low, high from %s_ticks where market = '%s' and symbol = '%s' order by time desc limit %d" % (market, market, symbol, size)
         rows = this.client.query(sql, epoch = 's')
