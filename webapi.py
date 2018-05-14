8e564059b78db038281467546094e292432369ae
Yang Yun <igouzy@live.com>
2018-05-14 16:06:42 +0800

    fix invalid key market_ticks_validator issue

--- webapi.py
+++ webapi.py
@@ -171,7 +171,7 @@ def tv_time():
 
 @app.route('/tv/config', methods=['GET'])
 def tv_config():
-    support_markets = [market for market in settings['markets'].keys() if not market in ['default', '_title', 'k10_daily_rank', 'k10_index_calc']]
+    support_markets = [market for market in settings['markets'].keys() if not market in ['default', '_title', 'k10_daily_rank', 'k10_index_calc', 'market_ticks_validator']]
     exchanges = [{ "value": "", "name": "All Exchanges", "desc": "" }]
     exchanges.extend([{
         "value": market,
@@ -216,7 +216,7 @@ def tv_search():
     if exchange:
         support_markets = [exchange]
     else:
-        support_markets = [market for market in settings['markets'].keys() if not market in ['default', '_title', 'k10_daily_rank', 'k10_index_calc']]
+        support_markets = [market for market in settings['markets'].keys() if not market in ['default', '_title', 'k10_daily_rank', 'k10_index_calc', 'market_ticks_validator']]
 
     matches = []
 
