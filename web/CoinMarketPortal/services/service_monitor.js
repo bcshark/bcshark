c97eb708d19c6ed0918e65dc647e591a3b3060d0
Yang Yun <igouzy@live.com>
2019-04-01 05:28:19 +0000

    remove web from bcshark repository

--- web/CoinMarketPortal/services/service_monitor.js
+++
@@ -1,9 +0,0 @@
-var MonitorService = ['$resource', '$http', 'ConfigService', function($resource, $http, config_service) {
-	var service = {};
-
-	service.monitor = function(callback) {
-        $resource(config_service.monitor).query({}, callback);
-	};
-
-	return service;
-}];
--- web/v1.0/services/service_symbol.js
+++
@@ -1,9 +0,0 @@
-var SymbolService = ['$resource', '$http', 'ConfigService', function($resource, $http, config_service) {
-	var service = {};
-
-    service.all = function(callback) {
-        $resource(config_service.symbols).query({}, callback);
-    };
-
-    return service;
-}];
